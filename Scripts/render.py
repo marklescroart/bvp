6d09eb88f39c1e42f029f9f081fdf077ad3d5e7e
Utkarsh Singhal <s.utkarsh@berkeley.edu>
2016-12-20 15:14:14 -0800

    action bg script and cosmetic changes to the code

--- Scripts/render.py
+++ Scripts/render.py
@@ -2,31 +2,37 @@
 Renders different backgrounds with different actions (Assuming actions are compatible with objects
 """
 
-#  Test
-
 import bvp
 dbi = bvp.DBInterface()
 
-script = """import bvp
-from bvp.Classes import SceneList
+bg_list = dbi.query(type='Background')
+act_list = dbi.query(type='Action', is_broken=False)
+for i in range(5):
+    bg = bg_list[i].name
+    for j in range(5):
+        act = act_list[j]. name
+        print("Rendering:", i,j,bg,act)
+        script = """import bvp
 dbi = bvp.DBInterface()
-ro = bvp.RenderOptions()
-# ro.BVPopts['Type'] = 'FirstAndLastFrame'
-ro.BVPopts['Type'] = 'All'
-bg = dbi.query(type='Background')[0]
 ob = dbi.query(semantic_category='human')[0]
-act = dbi.query(type='Action',is_broken=False)[0]
-
+ro = bvp.RenderOptions()
+ro.BVPopts['Type'] = 'FirstAndLastFrame'
+bg = dbi.query(type='Background', name= '"""+bg+"""')[0]
+act = dbi.query(type='Action', name= '"""+act+"""')[0]
 ob.action = act
 ob.size3D = ob.rot3D = ob.pos3D = None
+scn = bvp.Scene(objects=[ob], background=bg, sky=None, 
+                frame_range=(0,act.n_frames), 
+                fname='test_render_action_"""+str(i)+ "_" + str(j)+ """_####')
+try:
+    scn.populate_scene([ob],RaiseError=True,nIter=200)
+    scn.create(render_options=ro)
+    scn.render(ro)
+except Exception as e:
+    print("Unable to populate scene! Got error:", e)
+"""
 
-scn = bvp.Scene(objects=[ob], background=bg, sky=None, frame_range=(0,act.n_frames), fname='test_render_action_####')
-scn.populate_scene([ob],RaiseError=True,nIter=200)
-scn.create(render_options=ro)
 
-scn.render(ro)"""
-
-
-stdout, stderr = bvp.blend(script)
-print(str(stdout,'utf-8'))
-print(str(stderr,'utf-8'))
+        stdout, stderr = bvp.blend(script)
+        print(str(stdout,'utf-8'))
+        print(str(stderr,'utf-8'))
